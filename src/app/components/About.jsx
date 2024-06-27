a83fd25417afd1b881b744ea8e6e12cb4a060e8d
AdelGann <adelchaficgc@gmail.com>
2024-06-27 02:42:26 -0400

    latest

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -6,18 +6,18 @@ export const About = () => {
     <>
       <div className="about-container"> {/* Added a container class */}
         <div className="content mx-auto flex justify-center text-center py-56 lg:py-28 2xl:py-72">
-          <div className="bg-neutral-800 p-5 bg-opacity-30 rounded-lg shadow-lg shadow-neutral-900"> {/* Added bg-opacity-20 class */}
-            <div className="content-inner grid gap-10"> {/* Added content-inner class */}
+          <div className="bg-neutral-800 p-5 bg-opacity-30 rounded-xl shadow-xl shadow-neutral-900"> {/* Added bg-opacity-20 class */}
+            <div className="content-inner grid gap-10" > {/* Added content-inner class */}
               <div>
                 <div>
-                  <h2 className="text-xl sm:text-3xl font-bold">About</h2>
+                  <h2 className="text-xl sm:text-3xl font-bold" style={{fontFamily: "Fira Code"}}>About Me</h2>
                 </div>
                 <div>
                   <p className="pt-5 font-light text-white">{aboutMsg}</p>
                 </div>
               </div>
               <div>
-                <h3 className="text-xl sm:text-3xl font-bold">Contact</h3>
+                <h3 className="text-xl sm:text-3xl font-bold " style={{fontFamily: "Fira Code"}}>Contact Me</h3>
               </div>
             </div>
           </div>
