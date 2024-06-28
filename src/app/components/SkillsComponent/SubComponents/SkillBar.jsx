6fc939779fbca527a3cc3e3131e2551fbb5e2009
AdelGann <adelchaficgc@gmail.com>
2024-06-27 23:26:06 -0400

    Skills finished

--- src/app/components/SkillsComponent/SubComponents/SkillBar.jsx
+++ src/app/components/SkillsComponent/SubComponents/SkillBar.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import style from './SkillBar.module.css'
+import style from './SkillBar.module.css';
+
 const SkillBar = ({ icon, name, experience }) => {
   const progressPercentage = `${experience}%`;
 
   return (
     <div className="flex items-center gap-4 py-2">
-      <img src={icon} alt="" className='w-10'/>
+      <img src={icon} alt="" className="w-10" />
       <div className="flex-grow">
         <p className="font-medium">{name}</p>
         <div className="bg-gray-800 border-2 border-blue-800 rounded-md h-2 w-32 overflow-hidden mt-2">
-          <div className={`${style.contentInner} h-full rounded-sm bg-gradient-to-r from-stone-200 via-cyan-300 to-blue-500 `} style={{ width: progressPercentage, animation: "slideBar 2s ease"}} ></div>
-        </div> 
+          <div
+            className={`${style.contentInner} ${style.animateProgress}`}
+            style={{ width: progressPercentage }}
+          ></div>
+        </div>
       </div>
     </div>
   );
 };
 
 export default SkillBar;
+
